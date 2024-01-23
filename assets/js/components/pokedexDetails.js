fdb44da8299be95cd616c3857a98c9758f1ddcc9
Coudji <106971118+Coudji@users.noreply.github.com>
2024-01-23 17:56:23 +0100

    add blur effect to pokedex details

--- assets/js/components/pokedexDetails.js
+++ assets/js/components/pokedexDetails.js
@@ -1,34 +1,36 @@
-import { getPokedexEntryById, patchPokedexEntry } from "../utils/pokedex.js";
+import { getPokedexEntryById, patchPokedexEntry } from '../utils/pokedex.js';
 
-export default class PokedexDetail extends HTMLElement{
-    constructor(){
-        super();
-    }
+export default class PokedexDetail extends HTMLElement {
+	constructor() {
+		super();
+	}
 
-    connectedCallback(){
-        this.render();
-        this.setupListenners();
-    }
+	connectedCallback() {
+		this.render();
+		this.setupListenners();
+	}
 
-    render(){
-        const uid = this.getAttribute('uniqueid');
-        const pokemonData = getPokedexEntryById(uid);
-        const titleData= JSON.stringify({
-            pokedexId : pokemonData.pokedexId,
-            name : pokemonData.name,
-            fav: pokemonData.fav
-        });
+	render() {
+		const uid = this.getAttribute('uniqueid');
+		const pokemonData = getPokedexEntryById(uid);
+		const titleData = JSON.stringify({
+			pokedexId: pokemonData.pokedexId,
+			name: pokemonData.name,
+			fav: pokemonData.fav,
+		});
 
-        const typesMarkup = pokemonData.type.map((type)=> {
-            return `
+		const typesMarkup = pokemonData.type
+			.map((type) => {
+				return `
                 <div>
                     <img src="${type.image}">
                     <p>${type.name}</p>
                 </div>
             `;
-        }).join("");
+			})
+			.join('');
 
-        const statsMarkup = `
+		const statsMarkup = `
             <div>
                 <img src="./public/img/heart.svg" class="stats-img">
                 <p>${pokemonData.stats.HP}</p>
@@ -55,37 +57,43 @@ export default class PokedexDetail extends HTMLElement{
             </div>
         `;
 
-        const evolutionLevels = pokemonData.evoLine.reduce((levels, pokemon) => {
-            const level = pokemon.evolutionLevel || 0; 
-            if (!levels[level]) {
-                levels[level] = [];
-            }
-            levels[level].push(pokemon);
-            return levels;
-        }, {});
-        
-        const evolutionMarkup = Object.keys(evolutionLevels).map((level) => {
-            return `
+		const evolutionLevels = pokemonData.evoLine.reduce((levels, pokemon) => {
+			const level = pokemon.evolutionLevel || 0;
+			if (!levels[level]) {
+				levels[level] = [];
+			}
+			levels[level].push(pokemon);
+			return levels;
+		}, {});
+
+		const evolutionMarkup = Object.keys(evolutionLevels)
+			.map((level) => {
+				return `
                 <div>
                     <h3>Level ${level}</h3>
                     <div>
-                    ${evolutionLevels[level].map((pokemon) => {
-                        return `
+                    ${evolutionLevels[level]
+											.map((pokemon) => {
+												return `
                             <div>
                                 <img src="${pokemon.sprite}">
                                 <p>${pokemon.pokedexId} - ${pokemon.name}</p>
                             </div>
                         `;
-                    }).join("")}
+											})
+											.join('')}
                     </div>
                 </div>
             `;
-        }).join("");
+			})
+			.join('');
 
-        const displayMarkup = `
+		const displayMarkup = `
             <title-comp pkm-data='${titleData}'></title-comp>
             <div class="detailModal">
-                <img src=${pokemonData.image} alt="Image de ${pokemonData.name}" class="img-pokemon">
+                <img src=${pokemonData.image} alt="Image de ${
+			pokemonData.name
+		}" class="img-pokemon">
                 <div class="infoBox">
                     <div class="stats">
                         ${statsMarkup}
@@ -99,28 +107,31 @@ export default class PokedexDetail extends HTMLElement{
                 </div>
             </div>
             <div class="comment">
-                    <p class="showComment">${pokemonData.comment ? pokemonData.comment : 'Espace commentaire'}</p>
+                    <p class="showComment">${
+											pokemonData.comment ? pokemonData.comment : 'Espace commentaire'
+										}</p>
                     <textarea class="commentField" maxlength="151" placeholder="Appuyez sur 'Enter' pour valider."></textarea>
                     <img src="./public/img/back-arrow.svg" alt="Capturer Pokemon" id="btn-back">
             </div>
         `;
 
-        this.innerHTML = displayMarkup;
-    }
+		this.innerHTML = displayMarkup;
+	}
 
-    setupListenners(){
-        this.querySelector('#btn-back').addEventListener('click',()=>{
-            this.remove()
-        });
+	setupListenners() {
+		this.querySelector('#btn-back').addEventListener('click', () => {
+			this.parentNode.remove();
+			this.remove();
+		});
 
-        this.querySelector('.commentField').addEventListener("keypress",(event) => {
-            if(event.key === 'Enter'){
-                const uid = this.getAttribute('uniqueid');
-                event.preventDefault();
-                patchPokedexEntry(uid,{comment:`${event.target.value}`});
-                this.render();
-                this.setupListenners();
-            }
-        });
-    }
+		this.querySelector('.commentField').addEventListener('keypress', (event) => {
+			if (event.key === 'Enter') {
+				const uid = this.getAttribute('uniqueid');
+				event.preventDefault();
+				patchPokedexEntry(uid, { comment: `${event.target.value}` });
+				this.render();
+				this.setupListenners();
+			}
+		});
+	}
 }
--- assets/js/utils/pokedex.js
+++ assets/js/utils/pokedex.js
@@ -1,73 +1,73 @@
-import { getActiveUser, getUserById, patchUser } from "./user.js";
-import { updateTotalCount } from "./util.js";
+import { getActiveUser, getUserById, patchUser } from './user.js';
+import { updateTotalCount } from './util.js';
 
-
-function getPokedex(){
-    const pokedex = JSON.parse(localStorage.getItem('pokedex')) || [];
-    return pokedex
+function getPokedex() {
+	const pokedex = JSON.parse(localStorage.getItem('pokedex')) || [];
+	return pokedex;
 }
 
-export function getUserPokedex(userId){
-    const pokedex = getPokedex();
-    const filteredPokedex = pokedex.filter(pokemon => pokemon.ownerId === userId);
-    return filteredPokedex;
+export function getUserPokedex(userId) {
+	const pokedex = getPokedex();
+	const filteredPokedex = pokedex.filter((pokemon) => pokemon.ownerId === userId);
+	return filteredPokedex;
 }
 
-export function getUserSortedPokedex(userId){
-    const pokedex = getUserPokedex(userId);
-    pokedex.sort((a,b) => {
-        if (a.fav && !b.fav) {
-            return -1;
-        } else if (!a.fav && b.fav) {
-            return 1;
-        }
-        return a.pokedexId - b.pokedexId;
-    });
-    return pokedex
+export function getUserSortedPokedex(userId) {
+	const pokedex = getUserPokedex(userId);
+	pokedex.sort((a, b) => {
+		if (a.fav && !b.fav) {
+			return -1;
+		} else if (!a.fav && b.fav) {
+			return 1;
+		}
+		return a.pokedexId - b.pokedexId;
+	});
+	return pokedex;
 }
 
-export function getPokedexEntryById(uid){
-    const pokedex = getPokedex();
-    const filteredPokedex = pokedex.filter(pokemon => pokemon.uniqueId === Number(uid));
-    return filteredPokedex[0];
+export function getPokedexEntryById(uid) {
+	const pokedex = getPokedex();
+	const filteredPokedex = pokedex.filter((pokemon) => pokemon.uniqueId === Number(uid));
+	return filteredPokedex[0];
 }
 
-function savePokedex(pokedex){
-    localStorage.setItem('pokedex', JSON.stringify(pokedex));
+function savePokedex(pokedex) {
+	localStorage.setItem('pokedex', JSON.stringify(pokedex));
 }
 
-export function addToPokedex(pkmData){
-    const pokedex = getPokedex();
-    pokedex.push(pkmData);
-    savePokedex(pokedex);
+export function addToPokedex(pkmData) {
+	const pokedex = getPokedex();
+	pokedex.push(pkmData);
+	savePokedex(pokedex);
 }
 
-function getIndex(uid){
-    const pokedex = getPokedex();
-    const index = pokedex.findIndex(pokemon => pokemon.uniqueId === uid);
-    return index
+function getIndex(uid) {
+	const pokedex = getPokedex();
+	const index = pokedex.findIndex((pokemon) => pokemon.uniqueId === uid);
+	return index;
 }
 
-export function patchPokedexEntry(uid, updatedData){
-    const pokedex = getPokedex();
-    const indexToUpdate = getIndex(Number(uid));
-    pokedex[indexToUpdate] = { ...pokedex[indexToUpdate], ...updatedData };
-    savePokedex(pokedex);
+export function patchPokedexEntry(uid, updatedData) {
+	const pokedex = getPokedex();
+	const indexToUpdate = getIndex(Number(uid));
+	pokedex[indexToUpdate] = { ...pokedex[indexToUpdate], ...updatedData };
+	savePokedex(pokedex);
 }
 
-export function deleteOnePokedexEntry(uid){
-    const pokedex = getPokedex();
-    const indexToDelete = Number(getIndex(Number(uid)));
-    pokedex.splice(indexToDelete, 1);
-    savePokedex(pokedex);
+export function deleteOnePokedexEntry(uid) {
+	const pokedex = getPokedex();
+	const indexToDelete = Number(getIndex(Number(uid)));
+	pokedex.splice(indexToDelete, 1);
+	savePokedex(pokedex);
 }
 
-export function displayPokemonDetails(event){
-    const uid = Number(event.target.closest('pokedexcard-comp').getAttribute('uniqueid'));
-    const body = document.querySelector('body');
-    const modalContainer = document.createElement('pokedexdetail-comp');
-    modalContainer.setAttribute('uniqueid',uid);
-    body.appendChild(modalContainer);
+export function displayPokemonDetails(event) {
+	const uid = Number(event.target.closest('pokedexcard-comp').getAttribute('uniqueid'));
+	const body = document.querySelector('body');
+	const modalHolder = document.createElement('div');
+	const modalContainer = document.createElement('pokedexdetail-comp');
+	modalHolder.classList.add('hold');
+	modalContainer.setAttribute('uniqueid', uid);
+	modalHolder.appendChild(modalContainer);
+	body.appendChild(modalHolder);
 }
-
-
